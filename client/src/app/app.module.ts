c2b1f6335ce1e4c1c818154ef920611ed691fc8d
boy-eff <boyev.danila@gmail.com>
2022-04-08 16:15:56 +0300

    Merge branch 'main' of https://github.com/boy-eff/hello-world

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,21 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-=======
->>>>>>> f768fb874bf4260c0c924083c8604a148cf7558a
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HomeComponent,
     NavComponent
-=======
-    HomeComponent
->>>>>>> f768fb874bf4260c0c924083c8604a148cf7558a
   ],
   imports: [
     BrowserModule,
