122f31aae0da726d943f577a67d2839ddf565e17
boy-eff <boyev.danila@gmail.com>
2022-04-23 00:51:55 +0300

    Edit CollectionsController

--- API/DTO/CollectionDto.cs
+++ API/DTO/CollectionDto.cs
@@ -11,6 +11,6 @@ namespace API.DTO
         public int Id { get; set; }
         public string Name { get; set; }
         public string OwnerId { get; set; }
-        public ICollection<Word> Words { get; set; }
+        public ICollection<WordDto> Words { get; set; }
     }
 }
