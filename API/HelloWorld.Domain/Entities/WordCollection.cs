10f9ef090d4a396c56ac9b1d0a21edfebc1d2718
boy-eff <boyev.danila@gmail.com>
2022-04-28 18:45:02 +0300

    Edit collection entity

--- API/HelloWorld.Domain/Entities/WordCollection.cs
+++ API/HelloWorld.Domain/Entities/WordCollection.cs
@@ -4,6 +4,8 @@ namespace HelloWorld.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Theme { get; set; }
+        public string Description { get; set; }
         public string ReviewStatus { get; set; } = "Local";
         public ICollection<Word> Words { get; set; }
         public int OwnerId { get; set; }
