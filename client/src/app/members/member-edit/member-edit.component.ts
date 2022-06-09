258c27e8b57cdf11fe99172e506bc9f09ea3e548
boy-eff <boyev.danila@gmail.com>
2022-06-09 19:56:57 +0300

    Rename memberService to userService

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { UserInfo } from 'src/app/_models/user-info';
 import { UserAccessToken } from 'src/app/_models/user-access-token';
 import { AccountService } from 'src/app/_services/account.service';
-import { MemberService } from 'src/app/_services/member.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -15,18 +15,18 @@ export class MemberEditComponent implements OnInit {
   user: UserAccessToken;
   member$: Observable<UserInfo>;
   faPlusSquare = faPlusSquare;
-  constructor(private accountService: AccountService, private memberService: MemberService) { 
+  constructor(private accountService: AccountService, private userService: UserService) { 
     accountService.currentUser$.subscribe(user => this.user = user);
   }
 
   ngOnInit(): void {
-    this.member$ = this.memberService.getMemberByUsername(this.user.userName);
+    this.member$ = this.userService.getUserByUsername(this.user.userName);
     this.member$.subscribe(m => console.log(m));
   }
 
   handleFileInput(files: FileList) {
     let photo = files.item(0);
-    this.memberService.uploadPhoto(photo).subscribe();
+    this.userService.uploadPhoto(photo).subscribe();
   }
 
 }
